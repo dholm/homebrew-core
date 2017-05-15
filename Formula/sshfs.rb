d9a75720bf32bb362dd8a7f832b7e78001d71ace
David Holm <david.holm@ericsson.com>
2017-05-15 09:36:44 +0200

    sshfs: Only depends on osxfuse on OSX

--- Formula/sshfs.rb
+++ Formula/sshfs.rb
@@ -17,7 +17,7 @@ class Sshfs < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on :osxfuse
+  depends_on :osxfuse if OS.mac?
   depends_on "glib"
 
   def install
